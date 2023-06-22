3ce2129065e8f7ccb461f7192bdc540e1f6a69aa
MuhammadEzraMaulana <126476343+MuhammadEzraMaulana@users.noreply.github.com>
2023-06-22 20:31:29 +0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/TA-Film-Kel3-Eduwork/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'film_ref');
