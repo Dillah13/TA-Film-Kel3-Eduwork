4b59f7bb8dd20a462cde9e36d1f388e66d9b4cc0
MuhammadEzraMaulana <126476343+MuhammadEzraMaulana@users.noreply.github.com>
2023-06-22 20:14:09 +0700

    Delete app directory

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/TA-Film-Kel3-Eduwork/public');
-
-// DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'film_ref');
